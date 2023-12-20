7591f3fee2187848d1a45eddde83c80e166336f2
Zane <3277696440@qq.com>
2023-12-20 18:19:43 +0800

    feat✨: Request and upload file interceptor

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import '@/utils/http'
 
 const memberStore = useMemberStore()
+// 测试请求函数
+const getData = () => {
+  uni.request({
+    method: 'GET',
+    url: '/home/banner',
+    header: {},
+  })
+}
 </script>
 
 <template>
   <view class="my">
-    <view>会员信息：{{ memberStore.profile }}</view>
+    <view>Member Information: {{ memberStore.profile }}</view>
     <button
       @tap="
         memberStore.setProfile({
           nickname: 'Zanebla',
+          token: '12345',
         })
       "
       size="mini"
       plain
       type="primary"
     >
-      保存用户信息
+      Save User Information
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" plain type="primary">测试请求</button>
   </view>
 </template>
 
